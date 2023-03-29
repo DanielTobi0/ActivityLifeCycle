5ea62f969c2009833e0df3daad62f460d768208f
DanielTobi0 <adejumobidaniel563@gmail.com>
2023-03-29 16:24:13 +0100

    reformate files.

--- app/src/main/java/com/example/learnandroiddev/MainActivity.kt
+++ app/src/main/java/com/example/learnandroiddev/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.example.learnandroiddev
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.example.learnandroiddev.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,7 +14,32 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnAdd.setOnClickListener {}
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        println("On Start called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        println("On Resume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        println("On Pause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        println("On Stop called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        println("On Destroy called")
     }
 }
 
