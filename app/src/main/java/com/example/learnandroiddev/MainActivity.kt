79d6f0adc96cef073ca5a0ba8e9a90b406d60030
DanielTobi0 <adejumobidaniel563@gmail.com>
2023-03-29 16:35:56 +0100

    reformate files.

--- app/src/main/java/com/example/learnandroiddev/MainActivity.kt
+++ app/src/main/java/com/example/learnandroiddev/MainActivity.kt
@@ -6,40 +6,39 @@ import com.example.learnandroiddev.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-
-    private lateinit var binding: ActivityMainBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+    class MainActivity : AppCompatActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+        }
 
 
-    }
 
-    override fun onStart() {
-        super.onStart()
-        println("On Start called")
-    }
 
-    override fun onResume() {
-        super.onResume()
-        println("On Resume called")
-    }
+        override fun onStart() {
+            super.onStart()
+            println("On Start called")
+        }
 
-    override fun onPause() {
-        super.onPause()
-        println("On Pause called")
-    }
+        override fun onResume() {
+            super.onResume()
+            println("On Resume called")
+        }
 
-    override fun onStop() {
-        super.onStop()
-        println("On Stop called")
-    }
+        override fun onPause() {
+            super.onPause()
+            println("On Pause called")
+        }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        println("On Destroy called")
+        override fun onStop() {
+            super.onStop()
+            println("On Stop called")
+        }
+
+        override fun onDestroy() {
+            super.onDestroy()
+            println("On Destroy called")
+        }
     }
 }
 
